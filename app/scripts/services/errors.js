3e74a1360fb484232991ff776840314f39e7dad4
peterjurkovic <petojurkovic@gmail.com>
2015-05-04 10:23:02 +0100

    Remote error loggining

--- app/scripts/services/errors.js
+++ app/scripts/services/errors.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 angular.module('webdrilApp')
 .provider( "$exceptionHandler",{
     $get: function( errorLogService ) {
@@ -9,37 +8,15 @@ angular.module('webdrilApp')
   }
 ).factory("errorLogService", function( $log, $window, ENV) {
 
-    // I log the given error to the remote server.
     function log( exception, cause ) {
-
-      // Pass off the error to the default error handler
-      // on the AngualrJS logger. This will output the
-      // error to the console (and let the application
-      // keep running normally for the user).
       $log.error.apply( $log, arguments );
 
-      return !f ? [] :
-          st2(f.caller).concat([f.toString().split('(')[0].substring(9) + '(' + f.join(',') + ')']);
-      }
-
-      // Now, we need to try and log the error the server.
-      // --
-      // NOTE: In production, I have some debouncing
-      // logic here to prevent the same client from
-      // logging the same error over and over again! All
-      // that would do is add noise to the log.
       try {
-
-        // Log the JavaScript error to the server.
-        // --
-        // NOTE: In this demo, the POST URL doesn't
-        // exists and will simply return a 404.
-        console.log(st(exception));
-
-      
-
-
-
+        var trace = printStackTrace({e: exception}),
+            user = $window.sessionStorage.getItem('loggedUser');
+        if(user){
+          user= JSON.parse(user);
+        }
         $.ajax({
           type: "POST",
           url: ENV.api + '/errors',
@@ -47,15 +24,14 @@ angular.module('webdrilApp')
           data: angular.toJson({
             errorUrl: $window.location.href,
             errorMessage: exception.toString(),
-            stackTrace: JSON.stringify(exception),
+            stackTrace: trace.join('\n'),
             cause: ( cause || "" ),
-            version : ENV.version
+            version : ENV.version,
+            userId : user ? user.id : null
           })
         });
 
       } catch ( loggingError ) {
-
-        // For Developers - log the log-failure.
         $log.warn( "Error logging failed" );
         $log.log( loggingError );
 
